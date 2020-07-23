0a74389ca758757211dfb36b5cfcc017d450eefa
MarkiPro <marko.pasic.com@gmail.com>
2020-07-23 13:56:48 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,9 @@
 import discord
-from discord.ext import commands
-import requests
+from discord.ext import commands, tasks
+import aiohttp
 import json
 import random
+import asyncio
 
 class Fun(commands.Cog):
 
@@ -12,22 +13,22 @@ class Fun(commands.Cog):
     @commands.command()
     async def dog(self, ctx):
         message = await ctx.send("Generating a dog image for you!")
-        request = requests.get('https://dog.ceo/api/breeds/image/random')
-        data = request.json()
-        request.close()
-        embed = discord.Embed(title=f"A nice dog, for you!", colour=discord.Color.blurple())
-        embed.set_image(url=data['message'])
-        await ctx.send(embed=embed)
+        embed = discord.Embed(title="A nice dog for you!", color=0xe700ff)
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://dog.ceo/api/breeds/image/random') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                await ctx.send(embed=embed)
 
     @commands.command()
     async def cat(self, ctx):
-        await ctx.send("Generating a cat image for you!")
-        request = requests.get("https://api.thecatapi.com/v1/images/search", params={'limit': 1})
-        data = request.json()
-        request.close()
-        embed = discord.Embed(title=f"A nice cat, for you!", colour=discord.Color.blurple())
-        embed.set_image(url=data[0]['url'])
-        await ctx.send(embed=embed)
+        message = await ctx.send("Generating a cat image for you!")
+        embed = discord.Embed(title="A nice dog for you!", color=0xe700ff)
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://api.thecatapi.com/v1/images/search') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                await ctx.send(embed=embed)
 
 def setup(client):
     client.add_cog(Fun(client))
