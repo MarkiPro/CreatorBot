3ea3cd3f447afc2f971ad8da9ac1efb967df3e5a
MarkiPro <marko.pasic.com@gmail.com>
2020-07-24 19:47:52 +0200

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -10,6 +10,22 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @tasks.loop(seconds=3600)
+    async def send_meme(ctx: commands.Context):
+        channel = client.get_channel(id=712625666490761297)
+        embed = discord.Embed(title="A nice meme for you!", color=0xe700ff)
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+                res = await r.json()
+                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+                await channel.send(embed=embed)
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    @commands.cooldown(1, 3600, commands.BucketType.member)
+    async def meme_config(ctx):
+        send_meme.start(ctx=ctx)
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     @commands.cooldown(1, 5, commands.BucketType.member)
