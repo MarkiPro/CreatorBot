ad497222674240c6b2c425fdc800a61bcdfac75d
MarkiPro <marko.pasic.com@gmail.com>
2020-07-24 20:19:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -115,21 +115,21 @@ for file in os.listdir('cogs/'):
         print(f"LOADED {file}")
         client.load_extension(f'cogs.{file[:-3]}')
 
-    @tasks.loop(seconds=3600)
-    async def send_meme(ctx: commands.Context):
-        channel = client.get_channel(id=712625666490761297)
-        embed = discord.Embed(title="A nice meme for you!", color=0xe700ff)
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
-                res = await r.json()
-                embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
-                await channel.send(embed=embed)
+@tasks.loop(seconds=3600)
+async def send_meme(ctx: commands.Context):
+    channel = client.get_channel(id=712625666490761297)
+    embed = discord.Embed(title="A nice meme for you!", color=0xe700ff)
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await channel.send(embed=embed)
 
-    @client.command()
-    @commands.has_permissions(manage_messages=True)
-    @commands.cooldown(1, 3600, commands.BucketType.member)
-    async def meme_config(ctx):
-        send_meme.start(ctx=ctx)
+@client.command()
+@commands.has_permissions(manage_messages=True)
+@commands.cooldown(1, 3600, commands.BucketType.member)
+async def meme_config(ctx):
+    send_meme.start(ctx=ctx)
 
 @client.event
 async def on_member_join(member):
