c1b2f1b6f6b6da33a890eaf8be31bff4679fc10f
MarkiPro <marko.pasic.com@gmail.com>
2020-07-24 18:32:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -117,13 +117,7 @@ for file in os.listdir('cogs/'):
 
 @tasks.loop(seconds=10)
 async def send_meme(ctx: commands.Context):
-    channel = client.get_channel(id=712625666490761297)
-    embed = discord.Embed(title="A nice meme for you!", color=0xe700ff)
-    async with aiohttp.ClientSession() as cs:
-        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
-            res = await r.json()
-            embed.set_image(url=res[0]['url'])
-            await channel.send(embed=embed)
+    await ctx.send("spaghettios")
 
 @client.command()
 async def meme_config(ctx):
