a006a4c3fd5f0d00c2c05e06a7806bcdcc1e069d
MarkiPro <marko.pasic.com@gmail.com>
2020-07-27 13:09:24 +0200

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -12,25 +12,30 @@ class Fun(commands.Cog):
 
     @commands.command()
     async def dog(self, ctx):
-        message = await ctx.send("Generating a dog image for you!")
-        embed = discord.Embed(title="A nice dog for you!", color=0xe700ff)
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://dog.ceo/api/breeds/image/random') as image:
-                res_json = await image.json()
-                embed.set_image(url=res_json['message'])
-                await ctx.send(embed=embed)
-                print(res_json)
+        try:
+            message = await ctx.send("Generating a dog image for you!")
+            embed = discord.Embed(title="A nice dog for you!", color=0xe700ff)
+            async with aiohttp.ClientSession() as session:
+                async with session.get('https://dog.ceo/api/breeds/image/random') as image:
+                    res_json = await image.json()
+                    embed.set_image(url=res_json['message'])
+                    await ctx.send(embed=embed)
+        except:
+            return await ctx.send("There was an issue with loading the image.")
 
     @commands.command()
     async def cat(self, ctx):
-        message = await ctx.send("Generating a cat image for you!")
-        embed = discord.Embed(title="A nice cat for you!", color=0xe700ff)
-        async with aiohttp.ClientSession() as ca:
-            async with ca.get('https://api.thecatapi.com/v1/images/search') as c:
-                res2 = await c.json()
-                embed.set_image(url=res2[0]['url'])
-                await ctx.send(embed=embed)
-                print(res2)
+        try:
+            message = await ctx.send("Generating a cat image for you!")
+            embed = discord.Embed(title="A nice cat for you!", color=0xe700ff)
+            async with aiohttp.ClientSession() as ca:
+                async with ca.get('https://api.thecatapi.com/v1/images/search') as c:
+                    res2 = await c.json()
+                    embed.set_image(url=res2[0]['url'])
+                    await ctx.send(embed=embed)
+        except:
+            return await ctx.send("There was an issue with loading the image.")
+
 
 def setup(client):
     client.add_cog(Fun(client))
