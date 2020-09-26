8adfd04c7aedad08666b44427a1653260f499f6c
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-09-26 17:38:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -118,50 +118,6 @@ for file in os.listdir('cogs/'):
         client.load_extension(f'cogs.{file[:-3]}')
 
 @client.event
-async def on_member_join(member):
-    cc_guild = client.get_guild(id=611227128020598805)
-    ccs_guild = client.get_guild(id=729495109963612270)
-
-    if member.guild != ccs_guild:
-        return # this means that the member who joined didnt join CCS
-
-    member_in_cc = discord.utils.get(cc_guild.members, id=member.id)
-
-    role_definition = {712732582365757470: 729495109984321537, 722861996504121456: 729495109984321538, 722860980782235819: 729495109984321539, 722860699327397950: 729495109984321540, 722860570998603827: 729495109984321541, 722860303993405572: 729495109984321542, 722860184845680791: 729495109984321543, 722859531784421376: 729495109984321544, 722859441350770719: 729495109984321545, 722859802690060430: 729495109984321546, 722859343560704050: 729495109984321547, 722792418922987550: 729495109984321548, 695328029303635998: 729495109984321549, 722792492969492540: 729495110001361000, 722792338073714759: 729495110001361001, 695328300364464248: 729495110001361002, 722791896480612362: 729495110001361003, 722791558088491038: 729495110001361004, 722792808896921711: 729495110001361005, 695328208844881972: 729495110001361006}
-
-    if member_in_cc is None:
-        await member.kick(reason="Not in Content Creators.")
-
-    is_staff_member = False
-    # we will assume that every role in the role definition dict is a staff member, if they dont have any of the roles in CC, they get kicked|
-    for role in member_in_cc.roles:
-        if role.id in role_definition.keys(): #.keys() gives you the elements only of a dict.
-            is_staff_member = True
-
-    if not is_staff_member:
-        await member.kick(reason="Not staff member.")
-
-    for cc_role, ccs_role in role_definition.items():
-        for role in member_in_cc.roles:
-            if cc_role == role.id:
-                await member.add_roles(discord.utils.get(member.guild.roles, id=ccs_role))
-                SomeRandomEmbed20 = discord.Embed(
-                    title="**WELCOME MESSAGE**",
-                    description=f"*Welcome to the **{member.guild}** fellow staff member!*",
-                    color=0xffa200,
-                    timestamp=datetime.datetime.now(tz=None)
-                )
-                await member.send(embed=SomeRandomEmbed20)
-                return
-    SomeRandomEmbed = discord.Embed(
-        title="**WELCOME MESSAGE**",
-        description=f"*Welcome to the **{member.guild}**!*",
-        color=0xffa200,
-        timestamp=datetime.datetime.now(tz=None)
-    )
-    await member.send(embed=SomeRandomEmbed)
-
-@client.event
 async def on_command_error(ctx, error):
     embed = discord.Embed(
         title="**ERROR**",
@@ -169,5 +125,6 @@ async def on_command_error(ctx, error):
         color=0xff0000
     )
     await ctx.send(embed=embed)
+    
 
 client.run(token)
