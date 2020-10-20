f029c05545825ea08ad5449289822447ddf33cdc
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-10-20 17:20:33 +0200

    Update log.py

--- cogs/log.py
+++ cogs/log.py
@@ -1,4 +1,5 @@
-from discord import Forbidden
+import discord
+from discord import Embed
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -12,12 +13,34 @@ class Log(Cog):
             self.bot.cogs_ready.ready_up("log")
 
     @Cog.listener()
-    async def on_member_join(member):
-        message = discord.utils.get()
+    async def on_message_delete(self, before, after):
+        if not after.author.bot:
+            pass
 
     @Cog.listener()
-    async def on_member_remove(member):
+    async def on_message_edit(self, before, after):
+        if not after.author.bot:
+            pass
 
+    @Cog.listener()
+    async def on_member_update(self, before, after):
+        pass
+
+    @Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.bot.get_channel(767416354700918814)
+        message = channel.fetch_message(767422159270182922)
+        guild = channel.guild
+
+        await message.edit(content=f"Currently, there are a total **{guild.member_count}** Members in this server!!!")
+
+    @Cog.listener()
+    async def on_member_remove(self, ember):
+        channel = self.bot.get_channel(767416354700918814)
+        message = channel.fetch_message(767422159270182922)
+        guild = channel.guild
+
+        await message.edit(content=f"Currently, there are a total **{guild.member_count}** Members in this server!!!")
 
 def setup(bot):
     bot.add_cog(Log(bot))
