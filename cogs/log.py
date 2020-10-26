039ee0af7717d92b08c23926daaf8c2b15bc9cb7
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-10-26 20:34:27 +0100

    Update log.py

--- cogs/log.py
+++ cogs/log.py
@@ -3,6 +3,7 @@ from discord.ext.commands import Cog
 from discord.ext.commands import command
 import datetime
 
+
 class Log(Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,7 +15,11 @@ class Log(Cog):
 
     @Cog.listener()
     async def on_message_edit(self, before, after):
-        if not after.author.bot:
+        channel = self.bot.get_channel(767416354700918814)
+        message = await channel.fetch_message(767422159270182922)
+        log_channel = self.bot.get_channel(712624826463813753)
+
+        if not after.author.bot and before != message and after != message:
             log_embed = discord.Embed(
                 title="**Message Edit**",
                 description=f"{before.author} Edited The Message",
@@ -22,6 +27,7 @@ class Log(Cog):
             )
             log_embed.add_field(name="**Before**", value=f"{before}", inline=True)
             log_embed.add_field(name="**After**", value=f"{after}", inline=True)
+            await log_channel.send(embed=log_embed)
 
     @Cog.listener()
     async def on_member_update(self, before, after):
@@ -29,9 +35,11 @@ class Log(Cog):
         message = await channel.fetch_message(767422159270182922)
         guild = channel.guild
         booster_role = discord.utils.get(guild.roles, id=762172204628181023)
-        before_roles = ", ".join([i.mention for i in before.roles if i.name != '@everyone' or i.name != '­@­━━­━━━━ SKILLS ­━━­━━━━' or i.name != '­@━━━━━ ATTAINMENTS ━━━━­' or i.name != '@­━━━━━   ESTABLISHED  ━━━━­']) or "No roles assigned."
-        after_roles = ", ".join([i.mention for i in after.roles if i.name != '@everyone' or i.name != '­@­━━­━━━━ SKILLS ­━━­━━━━' or i.name != '­@━━━━━ ATTAINMENTS ━━━━­' or i.name != '@­━━━━━   ESTABLISHED  ━━━━­']) or "No roles assigned."
-        role_update_log_channel = self.bot.get_channel(712761099401035799)
+        before_roles = ", ".join([i.mention for i in before.roles if
+                                  i.name != '@everyone' or i.name != '­@­━━­━━━━ SKILLS ­━━­━━━━' or i.name != '­@━━━━━ ATTAINMENTS ━━━━­' or i.name != '@­━━━━━   ESTABLISHED  ━━━━­']) or "No roles assigned."
+        after_roles = ", ".join([i.mention for i in after.roles if
+                                 i.name != '@everyone' or i.name != '­@­━━­━━━━ SKILLS ­━━­━━━━' or i.name != '­@━━━━━ ATTAINMENTS ━━━━­' or i.name != '@­━━━━━   ESTABLISHED  ━━━━­']) or "No roles assigned."
+        role_update_log_channel = self.bot.get_channel(770368850679169075)
 
         if before_roles != after_roles:
             log_embed = discord.Embed(
@@ -44,11 +52,10 @@ class Log(Cog):
             await role_update_log_channel.send(embed=log_embed)
 
         if booster_role in before.roles and booster_role not in after.roles:
-            await message.edit(content=f"""Currently, there are a total of **{guild.member_count}** Members in this server,
+            await message.edit(
+                content=f"""Currently, there are a total of **{guild.member_count}** Members in this server,
                 **{guild.premium_subscription_count}** Boosters,
                 Boosting Level for this server is currently **{guild.premium_tier}**.""")
-
-
 
     @Cog.listener()
     async def on_member_join(self, member):
@@ -69,5 +76,7 @@ class Log(Cog):
         await message.edit(content=f"""Currently, there are a total of **{guild.member_count}** Members in this server,
             **{guild.premium_subscription_count}** Boosters,
             Boosting Level for this server is currently **{guild.premium_tier}**.""")
+
+
 def setup(bot):
     bot.add_cog(Log(bot))
