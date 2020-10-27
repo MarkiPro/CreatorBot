b6214932c75f03074784308ee56dc8bdea629cc6
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-10-27 13:26:31 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -259,7 +259,8 @@ class Misc(commands.Cog):
         embed.add_field(name="Account created on",
                         value=f"{user.created_at.strftime(format)} ({delta_created.days} days)", inline=True)
         embed.add_field(name="Nickname", value=f"{user.nick}", inline=True)
-        roles = ", ".join([i.mention for i in user.roles if i.name != '@everyone' or i.name != '@━━━━━━ SKILLS ━━━━━━' or i.name != '━━━━━ ATTAINMENTS ━━━━' or i.name != '@━━━━━ ESTABLISHED ━━━━']) or "No roles assigned."
+        exculded_roles = [611227128020598805, 707957214995808296, 732375953203789965, 743590325448212651]
+        roles = ", ".join(role.mention for role in user.roles if role.id not in exculded_roles)
         embed.add_field(name="Guild Roles", value=f"{roles}", inline=False)
         perms = ", ".join(
             list(i[0].title() for i in user.guild_permissions if i[1] == True)) or "No permissions in the guild."
