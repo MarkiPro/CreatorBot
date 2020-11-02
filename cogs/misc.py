49ac182f5c6ad35770efaf5aa78289248f0d3f8e
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-11-03 00:32:11 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -240,10 +240,11 @@ class Misc(commands.Cog):
                 return
             else:
                 some_channel = self.bot.get_channel(712625020567814157)
-                await some_channel.send("HmMmmMmmMmmMmMMmmMmMmmMmM")
+                await some_channel.send("111111111111111111111111111111111")
                 some_long_text = "blah blah blah blah test"
-                text_splitter = TextSplitter(char_per_page=200, text=some_long_text)
+                text_splitter = TextSplitter(char_per_page=1985, text=some_long_text)
 
+                await some_channel.send("222222222222222222222222222222")
                 for i, entry in enumerate(text_splitter.words_list):
                     prepared_embed = discord.Embed(
                         title="A simple Paginated thing")  # do not set the footer and descriping they get overriden.
@@ -253,7 +254,8 @@ class Misc(commands.Cog):
 
                     prepared_embed.description = discord.utils.escape_mentions(entry)
                     prepared_embed.set_footer(text=f"Page {i + 1} of {len(text_splitter.words_list)}")
-                    await some_channel.send(embed=prepared_embed)
+                await some_channel.send("33333333333333333333333333333333333")
+                await some_channel.send(embed=prepared_embed)
 
     @commands.command(aliases=["server-info", "si", "s-i", "guild-info", "guildinfo", "gi", "g-i", "server_info", "s_i", "guild_info", "g_i"], description="Displays basic information about the server.")
     async def serverinfo(self, ctx):
