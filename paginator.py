c274b3c59db3d27df8bbda96813003182ae1aad2
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-11-03 14:18:55 +0100

    Update paginator.py

--- paginator.py
+++ paginator.py
@@ -50,9 +50,9 @@ class Paginator:
 
             if i == 0:
                 prepared_embed.title = "**Hiring Post**"
-            if i == entry:
+            if i == len(self.words_list):
                 prepared_embed.timestamp = datetime.datetime.utcnow()
 
-            prepared_embed.footer = f"Page {i}/{entry}"
+            prepared_embed.footer = f"Page **{i + 1}**/**{len(self.words_list)}**"
 
             await channel.send(embed=prepared_embed)
