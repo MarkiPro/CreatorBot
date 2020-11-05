5511676fdb28f315d82d9d25161539f388e120bc
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-11-05 22:05:00 +0100

    Update paginator.py

--- paginator.py
+++ paginator.py
@@ -44,7 +44,7 @@ class Paginator:
         n = self.char_per_page
         self.words_list = [self.text[i:i + n] for i in range(0, len(self.text), n)]
 
-    async def send(self, bot, channel, end_channel=None, member=None, title=None, members=None):
+    async def send(self, bot, channel, end_channel=None, member=None, title=None):
         self.paginate()
         for i, entry in enumerate(self.words_list):
             prepared_embed = discord.Embed(description=entry, color=0x0064ff)
