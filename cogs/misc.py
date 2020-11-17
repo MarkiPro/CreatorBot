fb1aa4140f8c5cd307fff8ec99df0737fe847530
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-11-17 21:45:34 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -1124,7 +1124,7 @@ class Misc(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def role(self, ctx, role_name=None):
+    async def role(self, ctx, *, role_name=None):
         roles = {"former staff": 741728478721736755, "roblox verified": 741735258411499560, "verified": 695328817157373992, "devforum member": 742333164352962581, "devforum regular": 742333167133786124, "devforum top contributor": 742333168576888943, "devforum community champion": 742333169423876106, "first server booster": 712685506424471612, "server booster": 762172204628181023, "animator": 734662028353994752, "music composer": 735497558855516161, "Artist": 734662196025360416, "Clothing Designer": 734177621876801637, "UI Designer": 733274530524561430, "Translator": 734527264657637416, "gfx designer": 733280979921141821, "twitch streamer": 738814393663619182, "builder": 733281281671954442, "YouTuber": 738788543211634756, "3d modeler": 733281157872877629, "game designer": 733281086913773599, "css programmer": 741099963626422322, "xml programmer": 735147383716970507, "ruby programmer": 735147661430227025, "lua programmer": 732376072397783093, "php programmer": 732378321387520092, "c programmer": 732378157449216040, "c++ programmer": 732377777772167188, "js programmer": 732377955669508097, "java programmer": 732379273100263464, "c# programmer": 732377857317142650, "python programmer": 732377712286761001}
         chat_color_roles = {"former staff": 743013368511594569, "roblox verified": 743013370588037191, "verified": 732388199107657828, "devforum member": 743013366515236915, "devforum regular": 743013366880272474, "devforum top contributor": 743013367840768072, "devforum community champion": 743013368134107166, "server booster": 734527854871707762, "animator": 734664243038912552, "music composer": 735497751978311681, "Artist": 734664303327838230, "Clothing Designer": 734527130565738516, "UI Designer": 734150445764837466, "Translator": 734527217350082672, "gfx designer": 734150696944795698, "twitch streamer": 738814580712669214, "builder": 734149969292034208, "YouTuber": 735557139984285706, "3d modeler": 734527020905529375, "game designer": 732402691296198848, "programmer": 732387788493946881}
 
@@ -1440,6 +1440,8 @@ class Misc(commands.Cog):
                 else:
                     await ctx.send("You do not have the required role, please contact a staff member to assist you, or apply for the desired role using the `>apply` command.")
                     return
+            else:
+                await ctx.send("Role doesn't exist, or isn't provided in the dictionary.")
         else:
             await ctx.send("Get some help!")
 
