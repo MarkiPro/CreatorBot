5d3d26f0db9b8239e686ba9607cedcc7dd9b5a1d
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-12-09 19:50:54 +0100

    Update cooldown.py

--- cooldown.py
+++ cooldown.py
@@ -6,20 +6,9 @@ class Cooldown:
     def __init__(self, time):
         self.cooldown_start_time = time
 
-    async def time_it(self, user, message=None):
+    async def time_it(self, user):
         current_time = datetime.datetime.utcnow()
 
-        if message:
-            time_difference = (current_time - self.cooldown_start_time).total_seconds()
-            if time_difference < 0.25:
-                system_message = await message.channel.send(f"{user.mention}, you are sending messages too quickly!")
-                await message.delete()
-                await asyncio.sleep(10)
-                await system_message.delete()
-        else:
-            pass
-
-        if not message:
-            time_difference = (current_time - self.cooldown_start_time).total_seconds()
-            if time_difference < 3600:
-                await user.send("You are on cooldown for this category which lasts for 1 hour!")
+        time_difference = (current_time - self.cooldown_start_time).total_seconds()
+        if time_difference < 3600:
+            await user.send("You are on cooldown for this category which lasts for 1 hour!")
