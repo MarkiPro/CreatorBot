53d991ffc5f97707563a7363f8632495db5f2b58
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-12-12 20:33:38 +0100

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -49,7 +49,7 @@ class Verification(commands.Cog):
         text_color = color
         font_name = 'fonts/himalaya.ttf'
         font = ImageFont.truetype(font=font_name, size=50)
-        draw.text((50, 25), captcha_str, fill=text_color, font=font)
+        draw.text((25, 25), captcha_str, fill=text_color, font=font)
 
         for i in range(5, random.randrange(6, 10)):
             draw.line((getit(), getit()), fill=random.choice(fill_color), width=random.randrange(1, 3))
