488a3ed6fade474817b869ccd6fbc90c00d41f1d
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-12-12 21:16:01 +0100

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -52,7 +52,7 @@ class Verification(commands.Cog):
         w, h = draw.textsize(captcha_str, font)
         draw.text(((300 - w) / 2, (100 - h) / 2), captcha_str, fill=text_color, font=font)
 
-        for i in range(5, random.randrange(10, 25)):
+        for i in range(50, random.randrange(100, 300)):
             draw.line((random.randrange(0, 300), random.randrange(0, 100)), fill=random.choice(fill_color), width=random.randrange(8, 16))
 
         for i in range(50, random.randrange(100, 300)):
