8762af310b84aedec516006e8c233fcd217f46f5
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2020-12-12 18:47:31 +0100

    Update verification.py

--- cogs/verification.py
+++ cogs/verification.py
@@ -30,7 +30,7 @@ class Verification(commands.Cog):
 
         getit = lambda: (random.randrange(5, 85), random.randrange(5, 55))
 
-        colors = ["black", "red", "blue", "green", (64, 107, 76), (0, 87, 128), (0, 3, 82)]
+        color = (190, 190, 190)
 
         fill_color = [(64, 107, 76), (0, 87, 128), (0, 3, 82), (191, 0, 255), (72, 189, 0), (189, 107, 0), (189, 41, 0)]
 
@@ -47,17 +47,17 @@ class Verification(commands.Cog):
             img = Image.new('RGB', (90, 60), color=(95, 104, 222))
             draw = ImageDraw.Draw(img)
 
-            text_colors = random.choice(colors)
+            text_color = color
             font_name = "Chiller.ttf"
             font = ImageFont.truetype(font_name, 18)
-            draw.text((20, 20), captcha_str, fill=text_colors, font=font)
+            draw.text((20, 20), captcha_str, fill=text_color, font=font)
 
             for i in range(5, random.randrange(6, 10)):
                 draw.line((getit(), getit()), fill=random.choice(fill_color), width=random.randrange(1, 3))
 
             for i in range(10, random.randrange(11, 20)):
                 draw.point((getit(), getit(), getit(), getit(), getit(), getit(), getit(), getit(), getit(), getit()),
-                           fill=random.choice(colors))
+                           fill=color)
 
             img.save(file_name)
 
