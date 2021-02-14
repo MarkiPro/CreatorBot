956e867eec6944751e9ecad38e81075a3d085d7f
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2021-02-14 13:54:03 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -2485,7 +2485,7 @@ class Misc(commands.Cog):
             await ctx.author.send(embed=cancel_prompt_embed)
             return
         else:
-            with open("posts.json") as posts:
+            with open("CreatorBot/cogs/posts.json") as posts:
                 posts = json.loads(posts)
                 try:
                     category_json = posts[f"{category}"]
