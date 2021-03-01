f5a3a77d196d3ed3501c5a83e71e9572c4421a89
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2021-03-02 00:56:33 +0100

    Update misc.py

--- cogs/misc.py
+++ cogs/misc.py
@@ -2320,14 +2320,10 @@ class Misc(commands.Cog):
                 title = category_json["title"]
 
                 channel_id = category_json["channel"]
-                try:
-                    final_channel_id = category_json["end_channel"]
-                except:
-                    pass
+                final_channel_id = category_json["end_channel"]
 
                 channel = self.bot.get_channel(channel_id)
-                if final_channel_id:
-                    final_channel = self.bot.get_channel(final_channel_id)
+                final_channel = self.bot.get_channel(final_channel_id)
 
                 position = 1
                 for question in questions:
@@ -2354,13 +2350,8 @@ class Misc(commands.Cog):
                             print("hi noob")
                             pag = Paginator(post_text, 1985)
 
-                            if final_channel:
-                                await pag.send(bot=self.bot, channel=channel, end_channel=final_channel, member=ctx.author, title=title, mute_role=mute_role)
-                                print("aloha")
-                            else:
-                                print("yeeehaaaaaw")
-                                await pag.send(bot=self.bot, channel=channel, member=ctx.author, title=title, mute_role=mute_role)
-                            cooldown_category = Cooldown(time=datetime.datetime.utcnow())
+                            await pag.send(bot=self.bot, channel=channel, end_channel=final_channel, member=ctx.author, title=title, mute_role=mute_role)
+                            [cat_cooldown for category_cooldown, cat_cooldown in vars(self).items() if category_cooldown == f"{category}_cool"][0] = Cooldown(time=datetime.datetime.utcnow())
                             return
                         else:
                             print("nupe")
