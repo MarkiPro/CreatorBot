51dc22cc90e7dc7db0db377582f072701ad10382
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2021-03-13 12:15:39 +0100

    Update configuration.py

--- cogs/configuration.py
+++ cogs/configuration.py
@@ -17,33 +17,35 @@ class Configuration(commands.Cog):
 
     @add_config.command()
     async def banned_link(self, ctx, *, link):
-        with open("configs/banned.json") as banned_stuff_json:
-            banned_stuff = json.load(banned_stuff_json)
+        with open("configs/banned.json") as reader:
+            banned_stuff = json.load(reader)
             banned_links = banned_stuff["banned_links"]
 
             added_link = str(link)
 
             if added_link not in banned_links:
                 banned_links.append(added_link)
-                json.dump(banned_stuff, banned_stuff_json)
+                with open("configs/banned.json", "w") as writer:
+                    json.dump(banned_stuff, writer)
                 await ctx.send("Listed the link!")
             else:
-                await ctx.send("That link is already listed!")
+                return await ctx.send("That link is already listed!")
 
     @add_config.command()
     async def banned_word(self, ctx, *, word):
-        with open("configs/banned.json") as banned_stuff_json:
-            banned_stuff = json.load(banned_stuff_json)
+        with open("configs/banned.json", "r") as reader:
+            banned_stuff = json.load(reader)
             banned_words = banned_stuff["banned_words"]
 
             added_word = str(word)
 
             if added_word not in banned_words:
                 banned_words.append(added_word)
-                json.dump(banned_stuff, banned_stuff_json)
+                with open("configs/banned.json", "w") as writer:
+                    json.dump(banned_stuff, writer)
                 await ctx.send("Listed the word!")
             else:
-                await ctx.send("That word is already listed!")
+                return await ctx.send("That word is already listed!")
 
 
 def setup(bot):
