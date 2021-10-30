fe0dd7092700bb1d38f16d1f9d5332d26520cc50
MarkiPro <65014720+MarkiPro@users.noreply.github.com>
2021-10-30 17:26:25 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -2,10 +2,20 @@ import datetime
 import os
 import discord
 from discord.ext import commands
+from paginator import Paginator
 from webserver import keep_alive
 
 
 class EmbedHelpCommand(commands.MinimalHelpCommand):
+    """This is an example of a HelpCommand that utilizes embeds.
+    It's pretty basic but it lacks some nuances that people might expect.
+    1. It breaks if you have more than 25 cogs or more than 25 subcommands. (Most people don't reach this)
+    2. It doesn't DM users. To do this, you have to override `get_destination`. It's simple.
+    Other than those two things this is a basic skeleton to get you started. It should
+    be simple to modify if you desire some other behaviour.
+    To use this, pass it to the bot constructor e.g.:
+    bot = commands.Bot(help_command=EmbedHelpCommand())
+    """
 
     def __init__(self):
         super().__init__(command_attrs={
@@ -13,6 +23,7 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
         })
         self.dm_help = False
 
+    # Set the embed colour here
     COLOUR = 0x1E90FF
 
     def command_not_found(self, string):
@@ -25,17 +36,15 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
             return f"``{self.clean_prefix}{command.qualified_name}``"
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(title='**BOT COMMANDS**', colour=self.COLOUR, image=bot.user.avatar_url)
-        # description = f'**{self.context.bot.description}**'
 
         commands_dict = {}
 
-        for cog, commands in mapping.items():
+        for cog, bot_commands in mapping.items():
             name = 'Uncategorized' if cog is None else cog.qualified_name
-            filtered = await self.filter_commands(commands, sort=True)
+            filtered = await self.filter_commands(bot_commands, sort=True)
             if filtered:
                 commands_dict[name] = []
-                for c in commands:
+                for c in bot_commands:
                     commands_dict.get(f"{name}").append(c)
 
         description = '**``[]``: OPTIONAL, ``<>``: REQUIRED**\n\n'
@@ -45,16 +54,16 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
             for command in commands_dict.get(cog):
                 description = description + f"**{self.get_command_signature(command)}**\n\n"
 
-<<<<<<< HEAD:bot/bot.py
-        embed.description = description
-        await self.context.author.send(embed=embed)
-=======
+        pag = Paginator(description, 1985)
+
         await pag.send(bot=self.bot, channel=self.context.author, title='**BOT COMMANDS**')
->>>>>>> parent of 456d916 (Update bot.py):bot.py
 
     async def send_cog_help(self, cog):
-        embed = discord.Embed(title='{0.qualified_name} Commands'.format(cog), colour=self.COLOUR,
-                              image=bot.user.avatar_url)
+
+
+
+
+        embed = discord.Embed(title=f'{cog.qualified_name} Commands'.format(cog))
         if cog.description:
             embed.description = cog.description
 
@@ -62,11 +71,9 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
         for command in filtered:
             embed.add_field(name=self.get_command_signature(command), value=command.description or '...', inline=False)
 
-<<<<<<< HEAD:bot/bot.py
-        await self.get_destination().send(embed=embed)
-=======
-        await pag.send(bot=self.bot, channel=self.get_destination(), title=title)
->>>>>>> parent of 456d916 (Update bot.py):bot.py
+        pag = Paginator(description, 1985)
+
+        await pag.send(bot=self.bot, channel=self.get_destination(), title='**BOT COMMANDS**')
 
     async def send_group_help(self, group):
         embed = discord.Embed(title=group.qualified_name.upper(), colour=self.COLOUR, image=bot.user.avatar_url)
@@ -78,12 +85,8 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
             for command in filtered:
                 desc = desc + f'**{self.get_command_signature(command)}**\n\n'
 
-<<<<<<< HEAD:bot/bot.py
         embed.description = desc
         await self.get_destination().send(embed=embed)
-=======
-        await pag.send(bot=self.bot, channel=self.get_destination(), title=title)
->>>>>>> parent of 456d916 (Update bot.py):bot.py
 
     async def send_command_help(self, command):
         cog = 'Uncategorized' if command.cog is None else command.cog.qualified_name
@@ -95,27 +98,14 @@ class EmbedHelpCommand(commands.MinimalHelpCommand):
         no_aliases = 'This command has no aliases.'
 
         if isinstance(command, commands.Command):
-<<<<<<< HEAD:bot/bot.py
             embed.description = f"\n\n{self.get_command_signature(command)} - This is the correct usage of the ``{command.name}`` command. {command.description or no_desc}\n\nAliases: {command_aliases or no_aliases} "
             # embed.add_field(name=self.get_command_signature(command), value=command.description or 'No description  assigned.', inline=False)
-=======
-            description = f"\n\n{self.get_command_signature(command)} - This is the correct usage of the ``{command.name}`` command. {command.description or no_desc}\n\nAliases: {command_aliases or no_aliases} "
->>>>>>> parent of 456d916 (Update bot.py):bot.py
 
         await self.get_destination().send(embed=embed)
 
-<<<<<<< HEAD:bot/bot.py
-=======
-        await pag.send(bot=self.bot, channel=self.get_destination(), title=title)
->>>>>>> parent of 456d916 (Update bot.py):bot.py
 
 intents = discord.Intents.all()
 
-<<<<<<< HEAD:bot/bot.py
-=======
-intents = discord.Intents.all()
-
->>>>>>> parent of 456d916 (Update bot.py):bot.py
 bot = commands.Bot(commands.when_mentioned_or(">"), case_insensitive=True, help_command=EmbedHelpCommand(), intents=intents)
 
 
